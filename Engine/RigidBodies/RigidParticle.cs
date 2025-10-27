811b8f03a90e43b1f64ab28244bf54b8b2c5daa6
Camille Ferros <steam.mikoja@gmail.com>
2025-10-27 18:54:40 +0100

    a

--- Engine/RigidBodies/RigidParticle.cs
+++ Engine/RigidBodies/RigidParticle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Collision;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace Engine.RigidBodies
 {
-    internal class RigidParticle
+    public class RigidParticle : CollisionParticle
     {
+        public void SetState(
+        Vector3 position,
+        
+        float extents,
+        Vector3 velocity,
+        float mass=0.1f)
+        {
+            Body.Position = position;
+
+
+            Body.Velocity = velocity;
+            Body.Rotation = new();
+
+           
+
+            
+            Body.Mass = mass;
+
+            Matrix3 tensor = new();
+
+            Body.SetInertiaTensor(tensor);
+
+            Body.LinearDamping = 0.95f;
+            Body.AngularDamping = 0.8f;
+            Body.ClearAccumulators();
+            Body.Acceleration = new(0, -10f, 0);
+
+            Body.SetAwake();
+
+            Body.CalculateDerivedData();
+        }
     }
 }
