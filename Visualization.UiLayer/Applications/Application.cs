ace33bbcdc8514060c112a86f13d9aad61940e64
1180779 <rdkgsk@gmail.com>
2025-11-23 22:26:41 +0100

    Fix reset; maximize window

--- Visualization.UiLayer/Applications/Application.cs
+++ Visualization.UiLayer/Applications/Application.cs
@@ -15,281 +15,284 @@ namespace Visualization.UiLayer.Applications;
 
 public class Application : GameWindow
 {
-    private readonly ImGuiController imGuiController;
-    protected readonly IInputProvider InputProvider;
-    protected readonly QuadMesh QuadMesh;
-    protected readonly WindowFrameBuffer SceneRenderWindowFrb;
-    protected readonly WindowFrameBuffer DepthMapWindowFrb;
+	private readonly ImGuiController imGuiController;
+	protected readonly IInputProvider InputProvider;
+	protected readonly QuadMesh QuadMesh;
+	protected readonly WindowFrameBuffer SceneRenderWindowFrb;
+	protected readonly WindowFrameBuffer DepthMapWindowFrb;
 
-    private readonly ShadowSettingsWindow shadowSettingsWindow;
+	private readonly ShadowSettingsWindow shadowSettingsWindow;
 
-    protected readonly SceneManager Scene;
-    protected readonly Shader QuadShader;
-    // protected readonly FrameSaver FrameSaver = new(1000);
+	protected readonly SceneManager Scene;
+	protected readonly Shader QuadShader;
+	// protected readonly FrameSaver FrameSaver = new(1000);
 
-    protected const int DefaultWidth = 800;
-    protected const int DefaultHeight = 600;
-    protected const string DefaultTitle = "Display";
+	protected const int DefaultWidth = 800;
+	protected const int DefaultHeight = 600;
+	protected const string DefaultTitle = "Display";
 
-    public Application(int width = DefaultWidth, int height = DefaultHeight, string title = DefaultTitle) : base(
-        GameWindowSettings.Default,
-        new NativeWindowSettings())
-    {
-        Size = (width, height);
-        Title = title;
+	public Application(int width = DefaultWidth, int height = DefaultHeight, string title = DefaultTitle) : base(
+		GameWindowSettings.Default,
+		new NativeWindowSettings()
+		{
+			WindowState = WindowState.Maximized,
+		})
+	{
+		Size = (width, height);
+		Title = title;
 
-        imGuiController = new ImGuiController(this);
-        imGuiController.HookToWindow(this);
-        InputProvider = new ImGuiInputProvider(this, imGuiController);
+		imGuiController = new ImGuiController(this);
+		imGuiController.HookToWindow(this);
+		InputProvider = new ImGuiInputProvider(this, imGuiController);
 
-        SceneRenderWindowFrb = new(width, height);
-        DepthMapWindowFrb = new(width, height);
+		SceneRenderWindowFrb = new(width, height);
+		DepthMapWindowFrb = new(width, height);
 
-        QuadMesh = new();
+		QuadMesh = new();
 
-        QuadShader = new("depthMapShader.vert", "depthMapShader.frag");
-        Scene = new SceneLightningOnly(Size.X / (float)Size.Y);
+		QuadShader = new("depthMapShader.vert", "depthMapShader.frag");
+		Scene = new SceneLightningOnly(Size.X / (float)Size.Y);
 
-        // windows
-        shadowSettingsWindow = new ShadowSettingsWindow(() => this.Scene.LightsManager.DirectionalLight);
+		// windows
+		shadowSettingsWindow = new ShadowSettingsWindow(() => this.Scene.LightsManager.DirectionalLight);
 
-        // GL
-        GL.ClearColor(0.2f, 0.3f, 0.5f, 1f);
-        GL.Enable(EnableCap.DepthTest);
-    }
+		// GL
+		GL.ClearColor(0.2f, 0.3f, 0.5f, 1f);
+		GL.Enable(EnableCap.DepthTest);
+	}
 
-    protected bool StepsLimit { get; set; }
-    protected Int64 AvailableSteps { get; set; }
+	protected bool StepsLimit { get; set; }
+	protected Int64 AvailableSteps { get; set; }
 
-    protected bool DoUpdate
-    {
-        get
-        {
-            if (StepsLimit)
-            {
-                if (AvailableSteps > 0)
-                {
-                    AvailableSteps--;
-                    return true;
-                }
+	protected bool DoUpdate
+	{
+		get
+		{
+			if (StepsLimit)
+			{
+				if (AvailableSteps > 0)
+				{
+					AvailableSteps--;
+					return true;
+				}
 
-                return false;
-            }
+				return false;
+			}
 
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 
-    protected virtual void Update(float deltaTime)
-    {
-        if (!DoUpdate)
-            return;
-    }
+	protected virtual void Update(float deltaTime)
+	{
+		if (!DoUpdate)
+			return;
+	}
 
-    protected override void OnUpdateFrame(FrameEventArgs e)
-    {
-        base.OnUpdateFrame(e);
+	protected override void OnUpdateFrame(FrameEventArgs e)
+	{
+		base.OnUpdateFrame(e);
 
-        InputProvider.UpdateMousePosition();
+		InputProvider.UpdateMousePosition();
 
-        if (!IsFocused)
-        {
-            return;
-        }
+		if (!IsFocused)
+		{
+			return;
+		}
 
-        if (InputProvider.IsKeyPressed(InputKey.Equal))
-        {
-            DirectionalLightLayer++;
-        }
+		if (InputProvider.IsKeyPressed(InputKey.Equal))
+		{
+			DirectionalLightLayer++;
+		}
 
-        if (InputProvider.IsKeyPressed(InputKey.Minus))
-        {
-            DirectionalLightLayer--;
-        }
-    }
+		if (InputProvider.IsKeyPressed(InputKey.Minus))
+		{
+			DirectionalLightLayer--;
+		}
+	}
 
-    /// <summary>
-    /// This method is called after doing some initial setup.
-    /// It can overriden to add game objects to the initial scene. 
-    /// </summary>
-    protected virtual void InitializeScene()
-    {
-    }
+	/// <summary>
+	/// This method is called after doing some initial setup.
+	/// It can overriden to add game objects to the initial scene. 
+	/// </summary>
+	protected virtual void InitializeScene()
+	{
+	}
 
 
-    protected override void OnLoad()
-    {
-        base.OnLoad();
-        InitializeScene();
+	protected override void OnLoad()
+	{
+		base.OnLoad();
+		InitializeScene();
 
-        // set up internal scene objects after the scene is initialized
-        // this way some objects are already in the scene and can be accessed
-        // during the scene setup (e.g. attach camera to an object from demo)
-        Scene.SetUp();
-        Scene.Init(InputProvider);
-    }
+		// set up internal scene objects after the scene is initialized
+		// this way some objects are already in the scene and can be accessed
+		// during the scene setup (e.g. attach camera to an object from demo)
+		Scene.SetUp();
+		Scene.Init(InputProvider);
+	}
 
-    protected override void OnRenderFrame(FrameEventArgs args)
-    {
-        base.OnRenderFrame(args);
+	protected override void OnRenderFrame(FrameEventArgs args)
+	{
+		base.OnRenderFrame(args);
 
-        imGuiController.Update((float)args.Time);
+		imGuiController.Update((float)args.Time);
 
-        Update((float)args.Time); // Update game/app logic before any rendering
+		Update((float)args.Time); // Update game/app logic before any rendering
 
-        // --- ImGui Docking Setup ---
-        ImGuiWindowFlags dockspaceFlags = ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoCollapse |
-            ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoBringToFrontOnFocus |
-            ImGuiWindowFlags.NoNavFocus | ImGuiWindowFlags.NoBackground;
-        ImGuiViewportPtr viewport = ImGui.GetMainViewport();
-        ImGui.SetNextWindowPos(viewport.Pos);
-        ImGui.SetNextWindowSize(viewport.Size);
-        ImGui.SetNextWindowViewport(viewport.ID);
-        ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 0.0f);
-        ImGui.PushStyleVar(ImGuiStyleVar.WindowBorderSize, 0.0f);
-        ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new System.Numerics.Vector2(0.0f, 0.0f));
-        ImGui.Begin("DockSpace Host", dockspaceFlags);
-        ImGui.PopStyleVar(3);
+		// --- ImGui Docking Setup ---
+		ImGuiWindowFlags dockspaceFlags = ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoCollapse |
+			ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoBringToFrontOnFocus |
+			ImGuiWindowFlags.NoNavFocus | ImGuiWindowFlags.NoBackground;
+		ImGuiViewportPtr viewport = ImGui.GetMainViewport();
+		ImGui.SetNextWindowPos(viewport.Pos);
+		ImGui.SetNextWindowSize(viewport.Size);
+		ImGui.SetNextWindowViewport(viewport.ID);
+		ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 0.0f);
+		ImGui.PushStyleVar(ImGuiStyleVar.WindowBorderSize, 0.0f);
+		ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new System.Numerics.Vector2(0.0f, 0.0f));
+		ImGui.Begin("DockSpace Host", dockspaceFlags);
+		ImGui.PopStyleVar(3);
 
-        uint dockspaceId = ImGui.GetID("MyDockSpace");
-        ImGui.DockSpace(dockspaceId, new System.Numerics.Vector2(0, 0), ImGuiDockNodeFlags.PassthruCentralNode);
+		uint dockspaceId = ImGui.GetID("MyDockSpace");
+		ImGui.DockSpace(dockspaceId, new System.Numerics.Vector2(0, 0), ImGuiDockNodeFlags.PassthruCentralNode);
 
-        // draw windows here
-        HelpWindow.Draw();
-        RenderObjectInspectorWindow();
-        ShadowCascadingMapsWindow();
-        shadowSettingsWindow.Render();
-        RenderSceneWindow((float)args.Time);
+		// draw windows here
+		HelpWindow.Draw();
+		RenderObjectInspectorWindow();
+		ShadowCascadingMapsWindow();
+		shadowSettingsWindow.Render();
+		RenderSceneWindow((float)args.Time);
 
-        // end dockspace
-        ImGui.End();
+		// end dockspace
+		ImGui.End();
 
-        // clear the screen underneath the imGui windows (now background only)
-        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-        imGuiController.Render();
+		// clear the screen underneath the imGui windows (now background only)
+		GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+		imGuiController.Render();
 
-        SwapBuffers();
-    }
+		SwapBuffers();
+	}
 
-    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
-    {
-        base.OnFramebufferResize(e);
+	protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+	{
+		base.OnFramebufferResize(e);
 
-        GL.Viewport(0, 0, e.Width, e.Height);
-    }
+		GL.Viewport(0, 0, e.Width, e.Height);
+	}
 
-    protected override void OnUnload()
-    {
-        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-        GL.BindVertexArray(0);
-        GL.UseProgram(0);
+	protected override void OnUnload()
+	{
+		GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+		GL.BindVertexArray(0);
+		GL.UseProgram(0);
 
-        imGuiController.UnhookFromWindow(this);
-        SceneRenderWindowFrb.Dispose();
-        DepthMapWindowFrb.Dispose();
-        Scene.Dispose();
+		imGuiController.UnhookFromWindow(this);
+		SceneRenderWindowFrb.Dispose();
+		DepthMapWindowFrb.Dispose();
+		Scene.Dispose();
 
-        base.OnUnload();
-    }
+		base.OnUnload();
+	}
 
-    protected override void OnMouseWheel(MouseWheelEventArgs e)
-    {
-        base.OnMouseWheel(e);
+	protected override void OnMouseWheel(MouseWheelEventArgs e)
+	{
+		base.OnMouseWheel(e);
 
-        if (!ImGui.GetIO().WantCaptureMouse && Scene.CamerasManager.CameraMode)
-        {
-            Scene.CamerasManager.CurrentCamera.FovDegrees -= e.OffsetY;
-        }
-    }
+		if (!ImGui.GetIO().WantCaptureMouse && Scene.CamerasManager.CameraMode)
+		{
+			Scene.CamerasManager.CurrentCamera.FovDegrees -= e.OffsetY;
+		}
+	}
 
-    protected override void OnResize(ResizeEventArgs e)
-    {
-        base.OnResize(e);
+	protected override void OnResize(ResizeEventArgs e)
+	{
+		base.OnResize(e);
 
-        GL.Viewport(0, 0, Size.X, Size.Y);
-    }
+		GL.Viewport(0, 0, Size.X, Size.Y);
+	}
 
-    private int directionalLightLayer;
+	private int directionalLightLayer;
 
-    private int DirectionalLightLayer
-    {
-        get => directionalLightLayer;
-        set
-        {
-            if (Scene.LightsManager.DirectionalLight is null)
-                return;
+	private int DirectionalLightLayer
+	{
+		get => directionalLightLayer;
+		set
+		{
+			if (Scene.LightsManager.DirectionalLight is null)
+				return;
 
-            directionalLightLayer = value % (Scene.LightsManager.DirectionalLight.ShadowCascadeLevels.Length + 1);
-            if (directionalLightLayer < 0)
-            {
-                directionalLightLayer += (Scene.LightsManager.DirectionalLight.ShadowCascadeLevels.Length + 1);
-            }
-        }
-    }
+			directionalLightLayer = value % (Scene.LightsManager.DirectionalLight.ShadowCascadeLevels.Length + 1);
+			if (directionalLightLayer < 0)
+			{
+				directionalLightLayer += (Scene.LightsManager.DirectionalLight.ShadowCascadeLevels.Length + 1);
+			}
+		}
+	}
 
-    private void ShadowCascadingMapsWindow()
-    {
-        ImGui.Begin("Cascading Depth Maps");
+	private void ShadowCascadingMapsWindow()
+	{
+		ImGui.Begin("Cascading Depth Maps");
 
-        System.Numerics.Vector2 viewportSize = ImGui.GetContentRegionAvail();
-        var fbScale = imGuiController.ScaleFactor;
-        int fbWidth = Math.Max(1, (int)Math.Round(viewportSize.X * fbScale.X));
-        int fbHeight = Math.Max(1, (int)Math.Round(viewportSize.Y * fbScale.Y));
-        DepthMapWindowFrb.Resize(fbWidth, fbHeight);
+		System.Numerics.Vector2 viewportSize = ImGui.GetContentRegionAvail();
+		var fbScale = imGuiController.ScaleFactor;
+		int fbWidth = Math.Max(1, (int)Math.Round(viewportSize.X * fbScale.X));
+		int fbHeight = Math.Max(1, (int)Math.Round(viewportSize.Y * fbScale.Y));
+		DepthMapWindowFrb.Resize(fbWidth, fbHeight);
 
-        DepthMapWindowFrb.Bind();
-        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+		DepthMapWindowFrb.Bind();
+		GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-        QuadShader.Use();
-        Scene.LightsManager.DirectionalLight?.SetForDepthTextureShader(QuadShader, DirectionalLightLayer);
-        QuadMesh.Render();
+		QuadShader.Use();
+		Scene.LightsManager.DirectionalLight?.SetForDepthTextureShader(QuadShader, DirectionalLightLayer);
+		QuadMesh.Render();
 
-        DepthMapWindowFrb.Unbind();
+		DepthMapWindowFrb.Unbind();
 
-        GL.Viewport(0, 0, FramebufferSize.X, FramebufferSize.Y);
-        ImGui.Image(DepthMapWindowFrb.TextureId, viewportSize, new System.Numerics.Vector2(0, 1),
-            new System.Numerics.Vector2(1, 0)); // Flipped Y for OpenGL texture
-        ImGui.End();
-    }
+		GL.Viewport(0, 0, FramebufferSize.X, FramebufferSize.Y);
+		ImGui.Image(DepthMapWindowFrb.TextureId, viewportSize, new System.Numerics.Vector2(0, 1),
+			new System.Numerics.Vector2(1, 0)); // Flipped Y for OpenGL texture
+		ImGui.End();
+	}
 
-    private void RenderSceneWindow(float dt)
-    {
-        ImGui.Begin("Game Viewport");
+	private void RenderSceneWindow(float dt)
+	{
+		ImGui.Begin("Game Viewport");
 
-        bool isWindowHovered = ImGui.IsWindowHovered();
-        if (isWindowHovered)
-        {
-            Scene.ProcessInput(InputProvider, dt);
-        }
-        else
-        {
-            Scene.ProcessInputOutOfFocus(InputProvider, dt);
-        }
+		bool isWindowHovered = ImGui.IsWindowHovered();
+		if (isWindowHovered)
+		{
+			Scene.ProcessInput(InputProvider, dt);
+		}
+		else
+		{
+			Scene.ProcessInputOutOfFocus(InputProvider, dt);
+		}
 
-        System.Numerics.Vector2 viewportSize = ImGui.GetContentRegionAvail();
-        var fbScale = imGuiController.ScaleFactor;
-        int fbW = Math.Max(1, (int)Math.Round(viewportSize.X * fbScale.X));
-        int fbH = Math.Max(1, (int)Math.Round(viewportSize.Y * fbScale.Y));
-        SceneRenderWindowFrb.Resize(fbW, fbH);
-        Scene.RenderSceneWindow(fbW, fbH, SceneRenderWindowFrb);
+		System.Numerics.Vector2 viewportSize = ImGui.GetContentRegionAvail();
+		var fbScale = imGuiController.ScaleFactor;
+		int fbW = Math.Max(1, (int)Math.Round(viewportSize.X * fbScale.X));
+		int fbH = Math.Max(1, (int)Math.Round(viewportSize.Y * fbScale.Y));
+		SceneRenderWindowFrb.Resize(fbW, fbH);
+		Scene.RenderSceneWindow(fbW, fbH, SceneRenderWindowFrb);
 
-        SceneRenderWindowFrb.Unbind();
-        GL.Viewport(0, 0, FramebufferSize.X, FramebufferSize.Y);
-        ImGui.Image(SceneRenderWindowFrb.TextureId, viewportSize, new System.Numerics.Vector2(0, 1),
-            new System.Numerics.Vector2(1, 0));
-        ImGui.End();
-    }
+		SceneRenderWindowFrb.Unbind();
+		GL.Viewport(0, 0, FramebufferSize.X, FramebufferSize.Y);
+		ImGui.Image(SceneRenderWindowFrb.TextureId, viewportSize, new System.Numerics.Vector2(0, 1),
+			new System.Numerics.Vector2(1, 0));
+		ImGui.End();
+	}
 
-    private void RenderObjectInspectorWindow()
-    {
-        if (Scene.CamerasManager.CurrentCamera is FollowingCamera followingCamera)
-        {
-            var o = Scene.GameObjects.First(g => g.AbstractVisualObject == followingCamera.TargetObject);
-            ObjectInspectorWindow.Draw([o.PhysicsObject]);
-        }
-        else
-        {
-            ObjectInspectorWindow.Draw(Scene.GameObjects.Select(g => g.PhysicsObject).ToArray());
-        }
-    }
+	private void RenderObjectInspectorWindow()
+	{
+		if (Scene.CamerasManager.CurrentCamera is FollowingCamera followingCamera)
+		{
+			var o = Scene.GameObjects.First(g => g.AbstractVisualObject == followingCamera.TargetObject);
+			ObjectInspectorWindow.Draw([o.PhysicsObject]);
+		}
+		else
+		{
+			ObjectInspectorWindow.Draw(Scene.GameObjects.Select(g => g.PhysicsObject).ToArray());
+		}
+	}
 }
--- Visualization.UiLayer/Applications/Demos/BoxesFallingDemo.cs
+++ Visualization.UiLayer/Applications/Demos/BoxesFallingDemo.cs
@@ -8,152 +8,155 @@ namespace Visualization.UiLayer.Applications.Demos;
 
 public class BoxesFallingDemo : RigidBodyApplication
 {
-    /* The number of boxes in the simulation. */
-    private const uint Boxes = 0;
-    private ForceRegistry ForceRegistry = new ForceRegistry();
-    private Cloth Cloth;
-    private Box[] boxes = new Box[Boxes];
-    private Plane plane = null!;
+	/* The number of boxes in the simulation. */
+	private const uint Boxes = 0;
+	private ForceRegistry ForceRegistry = new ForceRegistry();
+	private Cloth Cloth;
+	private Box[] boxes = new Box[Boxes];
+	private Plane plane = null!;
 
-    protected override void InitializeScene()
-    {
-        Cloth = new Cloth(ForceRegistry);
+	protected override void InitializeScene()
+	{
+		Cloth = new Cloth(ForceRegistry);
 
-        /* add the cubes to the scene to be rendered */
-        for (var i = 0; i < Boxes; i++)
-        {
-            boxes[i] = new Box();
-            var box = boxes[i];
-            Scene.AddGameObject(box);
-        }
+		/* add the cubes to the scene to be rendered */
+		for (var i = 0; i < Boxes; i++)
+		{
+			boxes[i] = new Box();
+			var box = boxes[i];
+			Scene.AddGameObject(box);
+		}
 
-        Scene.AddGameObject(Cloth);
+		Scene.AddGameObject(Cloth);
 
-        /* add ground plane to the scene */
-        plane = new();
-        Scene.AddGameObject(plane);
+		/* add ground plane to the scene */
+		plane = new();
+		Scene.AddGameObject(plane);
 
-        /* set everything up */
-        Reset();
-    }
+		/* set everything up */
+		Reset();
+	}
 
-    /// <summary>
-    /// Processes the contact generation code.
-    /// </summary>
-    protected override void GenerateContacts()
-    {
-        // Note that this method makes a lot of uses of early returns to avoid
-        // processing lots of potential contacts that it hasn't got room to
-        // store.
+	/// <summary>
+	/// Processes the contact generation code.
+	/// </summary>
+	protected override void GenerateContacts()
+	{
+		// Note that this method makes a lot of uses of early returns to avoid
+		// processing lots of potential contacts that it hasn't got room to
+		// store.
 
-        // Set up the collision data structure
-        CollisionData.Reset(MaxContacts);
-        CollisionData.Friction = (Real)0.9;
-        CollisionData.Restitution = (Real)0.6;
-        CollisionData.Tolerance = (Real)0.1;
+		// Set up the collision data structure
+		CollisionData.Reset(MaxContacts);
+		CollisionData.Friction = (Real)0.9;
+		CollisionData.Restitution = (Real)0.6;
+		CollisionData.Tolerance = (Real)0.1;
 
-        // Perform exhaustive collision detection
-        for (var i = 0; i < Boxes; i++)
-        {
-            var box = boxes[i];
-            // Check for collisions with the ground plane
-            if (!CollisionData.HasMoreContacts()) return;
-            CollisionDetector.BoxAndHalfSpace(box.EngineBox, plane.EnginePlane, CollisionData);
+		// Perform exhaustive collision detection
+		for (var i = 0; i < Boxes; i++)
+		{
+			var box = boxes[i];
+			// Check for collisions with the ground plane
+			if (!CollisionData.HasMoreContacts()) return;
+			CollisionDetector.BoxAndHalfSpace(box.EngineBox, plane.EnginePlane, CollisionData);
 
-            // Check for collisions with each other box
-            for (var j = i + 1; j < Boxes; j++)
-            {
-                var other = boxes[j];
-                if (!CollisionData.HasMoreContacts()) return;
-                CollisionDetector.BoxAndBox(box.EngineBox, other.EngineBox, CollisionData);
+			// Check for collisions with each other box
+			for (var j = i + 1; j < Boxes; j++)
+			{
+				var other = boxes[j];
+				if (!CollisionData.HasMoreContacts()) return;
+				CollisionDetector.BoxAndBox(box.EngineBox, other.EngineBox, CollisionData);
 
-                if (IntersectionTests.BoxAndBox(box.EngineBox, other.EngineBox))
-                {
-                    box.EngineBox.IsOverlapping = other.EngineBox.IsOverlapping = true;
-                }
-            }
-        }
-        var engCloth = Cloth.EngineCloth;
+				if (IntersectionTests.BoxAndBox(box.EngineBox, other.EngineBox))
+				{
+					box.EngineBox.IsOverlapping = other.EngineBox.IsOverlapping = true;
+				}
+			}
+		}
 
-        for (int x = 0; x < engCloth.sizeX; x++)
-        {
-            for (int y = 0; y < engCloth.sizeY; y++)
-            {
-                if (!CollisionData.HasMoreContacts()) return;
+		var engCloth = Cloth.EngineCloth;
 
-                var rigidParticle = engCloth.particles[x, y];
-                if (rigidParticle == null || rigidParticle.Body == null) continue;
+		for (int x = 0; x < engCloth.sizeX; x++)
+		{
+			for (int y = 0; y < engCloth.sizeY; y++)
+			{
+				if (!CollisionData.HasMoreContacts()) return;
 
-                var collParticle = new CollisionParticle();
-                collParticle.Body = rigidParticle.Body;
-                collParticle.CalculateInternals();
+				var rigidParticle = engCloth.particles[x, y];
+				if (rigidParticle == null || rigidParticle.Body == null) continue;
 
-
-                if (!CollisionData.HasMoreContacts()) return;
-                CollisionDetector.ParticleAndHalfSpace(collParticle, plane.EnginePlane, CollisionData);
+				var collParticle = new CollisionParticle();
+				collParticle.Body = rigidParticle.Body;
+				collParticle.CalculateInternals();
 
 
-                for (var b = 0; b < Boxes; b++)
-                {
-                    if (!CollisionData.HasMoreContacts()) return;
-                    CollisionDetector.BoxAndParticle(boxes[b].EngineBox, collParticle, CollisionData);
-                }
-            }
-        }
-    }
+				if (!CollisionData.HasMoreContacts()) return;
+				CollisionDetector.ParticleAndHalfSpace(collParticle, plane.EnginePlane, CollisionData);
 
-    /// <summary>
-    /// Processes the objects in the simulation forward in time.
-    /// </summary>
-    protected override void UpdateObjects(float duration)
-    {
-        ForceRegistry.updateForces(duration);
-        Cloth.EngineCloth.Update(duration);
-        // Update the physics of each box in turn
-        for (var i = 0; i < Boxes; i++)
-        {
-            var box = boxes[i];
 
-            // Run the physics
-            box.EngineBox.Body.Integrate(duration);
-            box.EngineBox.CalculateInternals();
-            box.EngineBox.IsOverlapping = false;
-        }
-    }
+				for (var b = 0; b < Boxes; b++)
+				{
+					if (!CollisionData.HasMoreContacts()) return;
+					CollisionDetector.BoxAndParticle(boxes[b].EngineBox, collParticle, CollisionData);
+				}
+			}
+		}
+	}
 
-    /// <summary>
-    /// Resets the position of all the boxes and primes the explosion.
-    /// </summary>
-    protected override void Reset()
-    {
-        if (Boxes > 0)
-        {
-            boxes[0].EngineBox.SetState(
-                position: new Engine.Vector3(0, 3, 0),
-                orientation: new Engine.Quaternion(),
-                extents: new Engine.Vector3(4, 1, 1),
-                velocity: new Engine.Vector3(0, 0, 0)
-            );
-        }
+	/// <summary>
+	/// Processes the objects in the simulation forward in time.
+	/// </summary>
+	protected override void UpdateObjects(float duration)
+	{
+		ForceRegistry.updateForces(duration);
+		Cloth.EngineCloth.Update(duration);
+		// Update the physics of each box in turn
+		for (var i = 0; i < Boxes; i++)
+		{
+			var box = boxes[i];
 
-        if (Boxes > 1)
-        {
-            boxes[1].EngineBox.SetState(
-                position: new Engine.Vector3(0, 4.75f, 2),
-                orientation: new Engine.Quaternion(1.0f, 0.1f, 0.05f, 0.01f),
-                extents: new Engine.Vector3(1, 1, 4),
-                velocity: new Engine.Vector3(0, 0, 0)
-            );
-        }
+			// Run the physics
+			box.EngineBox.Body.Integrate(duration);
+			box.EngineBox.CalculateInternals();
+			box.EngineBox.IsOverlapping = false;
+		}
+	}
 
-        // Create the random objects
-        Random random = new();
-        for (var i = 2; i < Boxes; i++)
-        {
-            boxes[i].EngineBox.Random(random);
-        }
+	/// <summary>
+	/// Resets the position of all the boxes and primes the explosion.
+	/// </summary>
+	protected override void Reset()
+	{
+		Cloth.EngineCloth = new Engine.Cloth(ForceRegistry);
 
-        // Reset the contacts
-        CollisionData.ContactCount = 0;
-    }
+		if (Boxes > 0)
+		{
+			boxes[0].EngineBox.SetState(
+				position: new Engine.Vector3(0, 3, 0),
+				orientation: new Engine.Quaternion(),
+				extents: new Engine.Vector3(4, 1, 1),
+				velocity: new Engine.Vector3(0, 0, 0)
+			);
+		}
+
+		if (Boxes > 1)
+		{
+			boxes[1].EngineBox.SetState(
+				position: new Engine.Vector3(0, 4.75f, 2),
+				orientation: new Engine.Quaternion(1.0f, 0.1f, 0.05f, 0.01f),
+				extents: new Engine.Vector3(1, 1, 4),
+				velocity: new Engine.Vector3(0, 0, 0)
+			);
+		}
+
+		// Create the random objects
+		Random random = new();
+		for (var i = 2; i < Boxes; i++)
+		{
+			boxes[i].EngineBox.Random(random);
+		}
+
+		// Reset the contacts
+		CollisionData.ContactCount = 0;
+	}
 }
